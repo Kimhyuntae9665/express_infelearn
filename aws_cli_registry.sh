c119e45d735514c849d91bef4a1a5915e6cd6c0c
Kimhyuntae9665 <rlagusxo96652@naver.com>
2024-07-21 12:33:20 +0900

    auto_commit_20240721_123320

--- aws_cli_registry.sh
+++ aws_cli_registry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# 우분투 환경에서 실행되는 것처럼 바궈야하네, 지금은 powershell이라서, 근데 되기는 하네 
 powershell.exe -Command "Start-Process 'C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe'"
 
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 306593213077.dkr.ecr.ap-northeast-2.amazonaws.com
--- git_api_status.sh
+++ git_api_status.sh
@@ -9,13 +9,13 @@ TOKEN=$TOKEN  # 환경 변수에서 토큰을 가져옴
 response=$(curl -s -H "Authorization: token $TOKEN" \
     "https://api.github.com/repos/$REPO/actions/workflows/$WORKFLOW_NAME/runs?status=completed&per_page=1")
 
-# 최신 워크플로우의 상태 가져오기
+# 워크플로우의 상태 가져온다 
 workflow_status=$(echo $response | grep -o '"conclusion": "[^"]*' | grep -o '[^"]*$')
 
 # 워크플로우 상태 출력 (선택 사항)
 echo "Latest workflow status: $workflow_status"
 
-# 워크플로우가 성공적으로 완료된 경우 로컬 스크립트 실행
+# 워크플로우가 성공적으로 완료된 경우 aws_cli를 이용 registry에 docker image를 올리는 script를 진행 
 if [ "$workflow_status" == "success" ]; then
     echo "Workflow succeeded. Executing local script... Success"
     chmod +x ./aws_cli_registry.sh 
